50b334741eadbf04ef70b411bba9fa456fd99112
Loganius-II <132830555+Loganius-II@users.noreply.github.com>
2024-03-29 11:00:46 -0500

    Update client.py

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ SERVER = '192.168.1.140'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
+
 access = False
 while not access:
     name = input("Name: ")
@@ -14,29 +15,37 @@ while not access:
     check = client.recv(1024).decode()
     if check == "YOURGOODTOGO":
         access = True
-        break
     else:
+        print("Name already in use or contains a space. Please choose another name.")
 
-        print("Name already in use")
 try:
     def send(msg):
         message = msg.encode()
         client.send(message)
 
-    def recieve():
+    def receive():
         while True:
             try:
                 msg = client.recv(1024).decode()
+                print(msg)
             except:
-                print("[SERVER] Shutdown")
-            print(msg)
+                print("[SERVER] Disconnected from server")
+                print("You were either kicked or the server shutdown")
+                break
+
     connected = True
-    thread = threading.Thread(target=recieve, args=())
-    thread.start()
+    receive_thread = threading.Thread(target=receive)
+    receive_thread.start()
+
     while connected:
         mess = input("")
         send(mess)
         if mess == '!DISCNCT!':
             connected = False
+
+    print("[DISCONNECTED] Server shut down")
+    receive_thread.join()
+
 except:
     print("[DISCONNECTED] Server shut down")
+    receive_thread.join()
